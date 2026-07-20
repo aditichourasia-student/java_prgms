981dccf7a9e75a2556a09f5338eaf28dded60348
Aditi chourasia <aditichourasia764@gmail.com>
2026-07-20 15:18:52 +0530

    Initial commit

---
+++ src/java_prgms/Hello_world.java
@@ -0,0 +1,15 @@
+package java_prgms;
+
+public class Hello_world {
+
+	public Hello_world() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello, World! My fresh Eclipse works perfectly!");
+    
+	}
+
+}
