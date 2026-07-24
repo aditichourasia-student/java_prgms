cd32655136f29a3773ca915fd738b0c8a14a4426
aditichourasia-student <aditichourasia764@gmail.com>
2026-07-24 10:10:26 +0530

    Create Anagram.java

---
+++ src/java_prgms/Anagram.java
@@ -0,0 +1,14 @@
+package java_prgms;
+
+public class Anagram {
+
+	public Anagram() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
